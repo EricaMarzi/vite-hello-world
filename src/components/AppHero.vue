df30fe848c5167afbb6266322a390b29cfa2a2d1
EricaMarzi <erica.marzi.1993@gmail.com>
2024-01-31 18:28:18 +0100

    not work

---
+++ src/components/AppHero.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'AppHero',
+    data: () => ({
+        image: 'hello-banner.jpeg'
+    }),
+};
+</script>
+
+<template>
+    <img :src="`../../img${image}`" alt="">
+</template>
+
+<style>
+h1 {
+    text-align: center;
+}
+</style>
